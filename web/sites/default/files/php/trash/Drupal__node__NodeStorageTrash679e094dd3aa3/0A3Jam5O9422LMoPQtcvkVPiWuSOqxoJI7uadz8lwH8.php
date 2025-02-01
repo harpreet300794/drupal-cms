0f661ed337e98cff7936bfe48a99526981b46ff9
harpreet300794 <harpreetsinghsaluja30@gmail.com>
2025-02-01 17:25:46 +0530

    first commit

---
+++ web/sites/default/files/php/trash/Drupal__node__NodeStorageTrash679e094dd3aa3/0A3Jam5O9422LMoPQtcvkVPiWuSOqxoJI7uadz8lwH8.php
@@ -0,0 +1,12 @@
+<?php
+
+use Drupal\trash\TrashStorageTrait;
+
+/**
+ * Provides a custom storage class for trash-enabled entity types.
+ */
+class Drupal__node__NodeStorageTrash679e094dd3aa3 extends \Drupal\node\NodeStorage {
+
+  use TrashStorageTrait;
+
+}
